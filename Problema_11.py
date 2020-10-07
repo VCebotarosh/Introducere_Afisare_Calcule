ace3034f3f61df7ff650599458653e9ef8bb9304
Vladimir Cebotarosh <vladimir.cebotarosh@gmail.com>
2020-10-07 20:32:41 +0300

    fix stash

--- Problema_11.py
+++ Problema_11.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 """
 Măriuca  ţine  evidenţa  iepurilor  din  crescătorie.  
 Ea  îşi  notează  câţi  iepuri  sunt  la începutul fiecărei luni, 
@@ -11,18 +10,4 @@ să afişezela sfârşitul fiecărei luni câţi iepuri suntîn crescătorie?
 initial=int(input("Dati numarul de iepuri la inceput de luna: "))
 morti=int(input("Citi iepuri au murit: "))
 nascuti=int(input("Citi s-au nascut: "))
-=======
-"""
-Măriuca  ţine  evidenţa  iepurilor  din  crescătorie.  
-Ea  îşi  notează  câţi  iepuri  sunt  la începutul fiecărei luni, 
-câţi au murit şi câţi s-au născut în cursul fiecăei luni. 
-Puteţi să realizaţi un program care, primind aceste date, 
-să afişezela sfârşitul fiecărei luni câţi iepuri suntîn crescătorie?
- Exemplu: Date de intrare: nr. Iepuri la început de luna 10  
- nr. iepuri morti 2  nr. iepuri nascuti 6  Date de ieşire: 14 iepuri.  
-"""
-initial=int(input("Dati numarul de iepuri la inceput de luna: "))
-morti=int(input("Citi iepuri au murit: "))
-nascuti=int(input("Citi s-au nascut: "))
->>>>>>> Stashed changes
 print("In total in crescatorie sunt",initial-morti+nascuti,"iepuri")
